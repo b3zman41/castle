dc4a45fdc2344be94d054d2b6697dc944695b587
= <terencebezman@gmail.com>
2015-09-19 19:12:34 -0400

    Multiple answer support

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -19,7 +19,9 @@ class QuestionController extends Controller
 
     public function getQuestionByText(Request $request)
     {
-        return Question::where('question', md5($request->input('question')))->first();
+        return Question::where('question', md5($request->input('question')))
+            ->orderBy('agreements', 'desc')
+            ->first();
     }
 
     public function addQuestion(Request $request)
@@ -31,7 +33,7 @@ class QuestionController extends Controller
 
         if($validator->passes())
         {
-            $question = Question::where('question', md5($request->input('question')))->first();
+            $question = Question::where('question', md5($request->input('question')))->where('answer', $request->input('answer'))->first();
 
             if($question)
             {

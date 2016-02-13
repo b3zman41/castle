417f541d26232335e65b699671a6fecb82488f27
= <terencebezman@gmail.com>
2016-02-13 11:30:58 -0500

    Made migration for db to match new castle model

---
+++ database/migrations/2016_02_13_162226_update_question_model_to_fit_new_castle.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateQuestionModelToFitNewCastle extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropColumn('question');
+            $table->double('question_id')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropColumn('question_id');
+            $table->string('question');
+        });
+    }
+}
